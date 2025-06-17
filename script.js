9a61c12014e91f546126c23b03bbaea7899dc150
Divine-kuzo <d.kuzo@alustudent.com>
2025-06-17 23:51:46 +0200

    portfolio files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle navbar on mobile
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+});
